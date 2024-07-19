a7ff2e281b7abfb57397c0dedd9bb357946e4f24
Susom <susomshrestha@gmail.com>
2024-07-19 08:51:43 -0400

    install tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import Header from './Header.jsx';
+import './css/app.css';
 
 const App = () => {
   return (
     <div className='container'>
       <Header/>
-      <div className="container-view">Some activities should be here</div>
+      <div className="container-view">
+        <div>
+          <span className='text-3xl'>Hello</span>
+        </div>
+      </div>
     </div>
   );
 };
